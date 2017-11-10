6e67325ece6bc613b0c8acf2ef123cccc91ada1a
Abhinav Tushar <lepisma@fastmail.com>
2017-11-10 02:59:05 -0500

    Handle transformers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs-extra')
 const path = require('path')
+const glob = require('glob')
 const yaml = require('js-yaml')
 const shell = require('shelljs')
 const argv = require('yargs')
@@ -36,6 +37,8 @@ const readConfig = (configFile) => {
   }
 }
 
+const zip = rows => rows[0].map((_, c) => rows.map(row => row[c]))
+
 // ENTRY POINT
 let repo = argv.repo
 let message = argv.message || '[TRAVIS] Xpulled files from travis'
@@ -50,11 +53,24 @@ let localRepoPath = downloadRepo(fullRepoUrl(repo))
 config[repo].forEach(pattern => {
   // src is relative to downloaded repo
   let src = path.join(localRepoPath, pattern[0])
-  // Targe is relative to current repo root
+  let sfiles = glob.sync(src)
+  // Target is relative to current repo root
   let target = pattern[1]
   fs.ensureDirSync(target)
 
-  shell.cp('-RfL', src, target)
+  let tfiles
+  // Check for transformers
+  if (pattern.length === 3) {
+    let tf = eval(config.transformers[pattern[2]])
+    tfiles = sfiles.map(tf).map(f => path.join(target, f))
+  } else {
+    tfiles = sfiles.map(path.basename).map(f => path.join(target, f))
+  }
+
+  zip([sfiles, tfiles]).forEach(pair => {
+    shell.cp('-fL', pair[0], pair[1])
+  })
+
   console.log(`✓ Copied ${src}`)
   shell.exec(`git add ${target}`)
 })
